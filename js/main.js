6233d1d38c1c68a765cef0a45b026d4255440b87
Brian Wu <brianwu93@gmail.com>
2013-12-07 14:36:28 -0500

    new features

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var location_text;
 var itinerary = {};
 var results = [];
 var mapSearch = [];
+var map;
+var infowindows;
 
 // Constants
 var MAX_QUERY_SIZE = 15;
@@ -32,7 +34,11 @@ function parseVenues(venues) {
 		venue.location = new_venue["location"].address;
 		venue.lat = new_venue["location"].lat;
 		venue.long = new_venue["location"].lng;
-		venue.contact = new_venue["contact"].formattedPhone;
+		if (new_venue["contact"] && new_venue["contact"].hasOwnProperty("formattedPhone")) {
+			venue.contact = new_venue["contact"].formattedPhone;
+		} else {
+			venue.contact = "Phone Unavailable";
+		}
 		if (new_venue["hours"] && new_venue["hours"].hasOwnProperty("status")) {
 			venue.hours = new_venue["hours"].status;
 		} else {
@@ -55,18 +61,28 @@ function postVenuesInSearch(new_results) {
 	addToMap(new_results);
 	for (var i = 0; i < new_results.length; i++) {
 		$('#results').append("<a href=\"#\" class=\"list-group-item result" + i + "\">" + "<div><h4 style=\"display:inline;\">" + new_results[i].name + "</h4><span class=\"label label-info pull-right\">" + new_results[i].rating + "</span></div><div><h6 style=\"display:inline;\">" + new_results[i].location + "</h6><h6 class=\"pull-right\">" + new_results[i].hours + "</h6></div><h6 style=\"margin-top:3px;\">" + new_results[i].contact + "</h6>" + "</a>");
+		var name = ".result" + i;
+		$(name).click(function(key) {
+			return function() {
+				for (var i = 0; i < infowindows.length; i++) {
+					infowindows[i].close();
+				}
+				infowindows[key].open(map, mapSearch[key]);
+			}
+		}(i));
 	}
 	$('#search-panel').html("Currently Exploring: " + query_txt);
 }
 
 function addToMap(venues) {
 	mapSearch = [];
+	infowindows = [];
 	var myLatlng = new google.maps.LatLng(venues[0].lat,venues[0].long);
 	var mapOptions = {
 		zoom: 14,
 		center: myLatlng
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	for (var i = 0; i < venues.length; i++) {
 		var newLatlng = new google.maps.LatLng(venues[i].lat,venues[i].long);
 		// To add the marker to the map, use the 'map' property
@@ -76,6 +92,24 @@ function addToMap(venues) {
 		});
 		mapSearch[i] = marker;
 		mapSearch[i].setMap(map);
+
+		var content_string = "<a href=\"#\" class=\"list-group-item\"><h4 class=\"text-center\">" + venues[i].name + "</h4><h6 class=\"text-center\">" + venues[i].location + "</h6><h6 class=\"text-center\">" + venues[i].contact + "</h6><h6 class=\"text-center\">" + venues[i].hours + "</h6><h6 class=\"text-center\">" + venues[i].category + "</h6><button type=\"button\" class=\"btn btn-info btn-lg btn-block\">Add</button></a>";
+
+		var infowindow = new google.maps.InfoWindow({
+			content: content_string,
+			maxWidth: 1000
+		});
+		infowindows[i] = infowindow;
+
+		google.maps.event.addListener(mapSearch[i], 'click', function(innerKey) {
+			return function() {
+				for (var i = 0; i < infowindows.length; i++) {
+					infowindows[i].close();
+				}
+				infowindows[innerKey].open(map, mapSearch[innerKey]);
+			};
+			//console.log(i);
+		}(i));
 	}
 }
 
@@ -102,6 +136,14 @@ search_button.click(function() {
 
 });
 
+// Drag functionality.
+$(function () {
+    $("#itinerary").sortable({
+        tolerance: 'pointer',
+        revert: 'invalid',
+        forceHelperSize: true
+    });
+});
 
 
 
